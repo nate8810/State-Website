6728fb0adefb266e3926c791ee9c0c987d57088b
Darth Nater <ncast10@wgu.edu>
2024-04-22 20:13:02 -0700

    Add project files.

---
+++ scripts/contact.js
@@ -0,0 +1,18 @@
+﻿//D.1: Includ js to validate email
+function confirmEmail(e) {
+    let alert = document.getElementById('submitSuccess');
+    let email = document.getElementById('email');
+    let confirm = document.getElementById('emailConfirm');
+    alert.style.display = 'none';
+    confirm.classList.remove('is-invalid');
+    if (email.value != confirm.value) {
+        confirm.classList.add('is-invalid');
+    }
+    else {
+        alert.style.display = '';
+    }
+    e.preventDefault();
+    e.stopPropagation();
+}
+
+document.getElementById('contactForm').addEventListener('submit', e => confirmEmail(e));
